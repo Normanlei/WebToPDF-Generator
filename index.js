cc79e9dfc6341a6f38fcc72457499784892def71
Norman Lei <normanlei0901@gmail.com>
2019-11-14 00:44:11 -0800

    working

---
+++ index.js
@@ -0,0 +1,68 @@
+const questions = [
+  {
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "username"
+  },
+  {
+    type: "list",
+    message: "Pick your favorite color theme:",
+    name: "color",
+    choices: ["green","blue","pink","red"]
+  }
+];
+const util = require ("util");
+const pdf = require ("html-pdf");
+const axios = require ("axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./generateHTML");
+let githubURL;
+let username;
+let usercolor;
+let htmlfile;
+let html;
+
+function covertToPDF (html, pdfname){
+    var options = { format: 'A4' };
+    pdf.create(html, options).toFile(pdfname, function(err, res) {
+        if (err) return console.log(err);
+        console.log(res);
+      });
+}
+
+
+
+async function writeToFile(fileName, data) {
+    let writefileAsync = util.promisify(fs.writeFile);
+    await writefileAsync(fileName,data);
+    html = fs.readFileSync('./index.html', 'utf8');
+    covertToPDF(html,"index.pdf");
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput){
+        username = userInput.username;
+        usercolor = userInput.color;
+        username = username.toLowerCase().trim();
+        // console.log(usercolor);
+        // console.log(username);
+        githubURL = `https://api.github.com/users/${username}`;
+        return githubURL;
+    })
+    .then(function(githubURL){
+        callAxios(githubURL);
+    });
+}
+
+function callAxios(url){
+    axios.get(url)
+    .then(function(response){
+        response.data.color = usercolor; //adding data color property
+        htmlfile = generateHTML(response.data);
+        writeToFile("index.html",htmlfile);
+    })
+}
+
+init();
